9bd4ebb1e1fae943e46f7d89d34666be0fe3ecde
Your Name <rghzaiel@ggn-group.com>
2023-01-05 10:04:11 +0100

    users list

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 import AddUser from './components/users/addUser';
+import UsersList from './components/users/usersList';
 
 function App() {
+  const [usersList,setUsersList] = useState([]);
+  const addUserHandler = (uName, uAge) =>{
+    setUsersList(prevUsersList => 
+      ([...prevUsersList, {id:Math.random().toString(), username: uName, age: uAge}])
+      );
+  }
   return (
-    <AddUser />
+    <div>
+      <AddUser onAddUser={addUserHandler}/>
+      <UsersList users={usersList}/>
+    </div>
   );
 }
 
---
+++ src/components/users/usersList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Card from "../UI/card";
+import classes from './usersList.module.css';
+
+const UsersList = props => {
+
+    return(
+        <Card className={classes.users}>
+        <ul>
+        {
+            props.users.map(user => (
+                <li key={user.id} >{user.username} {user.age}</li>
+            ))
+        }
+        </ul>
+        </Card>
+    );
+
+}
+
+export default UsersList;
